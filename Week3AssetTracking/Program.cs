d24c4db34ae7d8a3c75838e0ef84d99bda96a392
Martina <a.martina.eriksson@hotmail.com>
2023-11-15 14:51:02 +0100

    feat: adds list of assets

--- Week3AssetTracking/Program.cs
+++ Week3AssetTracking/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Week3AssetTracking;
+
+List<Asset> assetList = new List<Asset> {
+    new Computer("ASUS ROG", "B550-F", new DateTime(2020, 11, 24), 2438),
+    new Computer("HP", "14S-FQ1010NO", new DateTime(2022, 01, 30), 6790),
+    new Phone("Samsung", "S20 Plus", new DateTime(2020, 09, 12), 15000),
+    new Phone("Sony Xperia", "10 III", new DateTime(2020, 03, 06), 8000)
+};
+
+foreach (var asset in assetList)
+{
+    Console.WriteLine(asset.EndOfLife);
+}
