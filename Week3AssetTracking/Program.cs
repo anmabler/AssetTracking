dcf674afa0d010082e236ce740d2722e832a27f4
Martina <a.martina.eriksson@hotmail.com>
2023-11-16 10:00:14 +0100

    feat: order by on list

--- Week3AssetTracking/Program.cs
+++ Week3AssetTracking/Program.cs
@@ -4,7 +4,8 @@ List<Asset> assetList = new List<Asset> {
     new Computer("ASUS ROG", "B550-F", new DateTime(2020, 11, 24), 2438),
     new Computer("HP", "14S-FQ1010NO", new DateTime(2022, 01, 30), 6790),
     new Phone("Samsung", "S20 Plus", new DateTime(2020, 09, 12), 15000),
-    new Phone("Sony Xperia", "10 III", new DateTime(2020, 03, 06), 8000)
+    new Phone("Sony Xperia", "10 III", new DateTime(2020, 03, 06), 8000),
+    new Computer("HP", "Elitebook", new DateTime(2021, 08, 30), 22340)
 };
 //Asset.addMultiple(assetList);
 
@@ -17,7 +18,8 @@ void displayList()
     
     Console.WriteLine("Type".PadRight(15) + "Brand".PadRight(15) + "Model".PadRight(15) + "Purchase Date".PadRight(15) + "Price");
     Console.WriteLine("----------------------------------------------------------------------------------------------------");
-
+    // sort alphabetical - then purchase date ascending
+    assetList = assetList.OrderBy(asset => asset.GetType().Name).ThenBy(asset => asset.PurchaseDate).ToList();
     foreach (Asset asset in assetList)
     {
         Console.WriteLine(asset.GetType().Name.PadRight(15) + asset.Brand.PadRight(15)+ asset.Model.PadRight(15) + asset.PurchaseDate.ToShortDateString().PadRight(15) + asset.Price.ToString().PadRight(15));
