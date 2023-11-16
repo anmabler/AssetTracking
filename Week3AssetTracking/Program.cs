0b8f310355eca11073b1c7293d3f105b4a8c9087
Martina <a.martina.eriksson@hotmail.com>
2023-11-16 10:39:14 +0100

    feat: colorized output from displayList

--- Week3AssetTracking/Program.cs
+++ Week3AssetTracking/Program.cs
@@ -5,7 +5,9 @@ List<Asset> assetList = new List<Asset> {
     new Computer("HP", "14S-FQ1010NO", new DateTime(2022, 01, 30), 6790),
     new Phone("Samsung", "S20 Plus", new DateTime(2020, 09, 12), 15000),
     new Phone("Sony Xperia", "10 III", new DateTime(2020, 03, 06), 8000),
-    new Computer("HP", "Elitebook", new DateTime(2021, 08, 30), 22340)
+    new Phone("Iphone", "10", new DateTime(2018, 11, 25), 9512),
+    new Computer("HP", "Elitebook", new DateTime(2021, 08, 30), 22340),
+    new Computer("HP", "Elitebook", new DateTime(2020, 07, 30), 22340)
 };
 //Asset.addMultiple(assetList);
 
@@ -22,6 +24,26 @@ void displayList()
     assetList = assetList.OrderBy(asset => asset.GetType().Name).ThenBy(asset => asset.PurchaseDate).ToList();
     foreach (Asset asset in assetList)
     {
-        Console.WriteLine(asset.GetType().Name.PadRight(15) + asset.Brand.PadRight(15)+ asset.Model.PadRight(15) + asset.PurchaseDate.ToShortDateString().PadRight(15) + asset.Price.ToString().PadRight(15));
+        TimeSpan timeSpan = DateTime.Now - asset.EndOfLife;
+        
+        Console.WriteLine(timeSpan.Days);
+        if (timeSpan.Days >= 180)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(asset.GetType().Name.PadRight(15) + asset.Brand.PadRight(15) + asset.Model.PadRight(15) + asset.PurchaseDate.ToShortDateString().PadRight(15) + asset.Price.ToString().PadRight(15));
+            Console.ResetColor();
+        }
+        else if (timeSpan.Days > 0 && timeSpan.Days >= 90)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(asset.GetType().Name.PadRight(15) + asset.Brand.PadRight(15) + asset.Model.PadRight(15) + asset.PurchaseDate.ToShortDateString().PadRight(15) + asset.Price.ToString().PadRight(15));
+            Console.ResetColor();
+        }
+        
+        else
+        { 
+            Console.WriteLine(asset.GetType().Name.PadRight(15) + asset.Brand.PadRight(15)+ asset.Model.PadRight(15) + asset.PurchaseDate.ToShortDateString().PadRight(15) + asset.Price.ToString().PadRight(15));
+        }
+
     }
 }
